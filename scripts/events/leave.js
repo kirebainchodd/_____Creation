a9c61b79c2f1292f06686a8410fd7f5e43140c73
kirebainchodd <itachithegay@gmail.com>
2024-08-30 17:29:49 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -25,7 +25,7 @@ module.exports = {
 			session4: "evening",
 			leaveType1: "left",
 			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
+			defaultLeaveMessage: "{userName}আবাল গ্রুপ এ থাকার যোগ্য না। তাই -- {type} the group"
 		}
 	},
 
